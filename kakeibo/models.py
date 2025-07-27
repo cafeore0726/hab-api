47167069b18000203fe8f5486717bff8df0a1ddd
reo0726 <kafeore0726ri@docomo.ne.jp>
2025-07-27 13:31:52 +0900

    モデルの作成

--- kakeibo/models.py
+++ kakeibo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Record(models.Model):
+    name = models.CharField(max_length=100)  # 項目名
+    amount = models.IntegerField()           # 金額（支出はマイナス）
+    recorded_at = models.DateTimeField(auto_now_add=True)  # 記録日時
+
+    def __str__(self):
+        return f"{self.name} - {self.amount}円"
