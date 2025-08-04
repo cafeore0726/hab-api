3f4d73145b9c4f75a999736718df94964ba870e1
reo0726 <kafeore0726ri@docomo.ne.jp>
2025-08-04 23:08:07 +0900

    モデルの作成

--- kakeibo/models.py
+++ kakeibo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Record(models.Model):
+    name = models.CharField(max_length=100)
+    amount = models.IntegerField()
+    recorded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.amount}円)"
